c0348d8b5ae7caab290f098f202c77ca63a865c7
Crend King <975235+CrendKing@users.noreply.github.com>
2026-03-27 19:23:15 -0700

    Optimize dependencies

--- Build.py
+++ Build.py
@@ -1,11 +1,14 @@
+import os
 import shutil
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.check_call(['uv.exe', 'run', '.venv/Scripts/gftools-builder.py', 'sources/config.yaml'])
+    subprocess.check_call(['gftools', 'builder', 'sources/config.yaml'])
 
     # autohint for personal taste
     # since we have to use a earlier version of gftools to build fonts, which doesn't include the autohint script, run it from another venv
-    subprocess.run(['uv.exe', '--project', 'D:/Development/Python/Font', 'run', 'gftools', 'autohint', '--auto-script', 'fonts/variable/Inconsolata[wdth,wght].ttf'])
+    subprocess.run(['uv', '--project', 'D:/Development/Python/Font', 'run', 'gftools', 'autohint', '--auto-script', 'fonts/variable/Inconsolata[wdth,wght].ttf'])
 
+    os.remove('sources/build.ninja')
+    os.remove('sources/.ninja_log')
     shutil.rmtree('sources/master_ufo', ignore_errors=True)
